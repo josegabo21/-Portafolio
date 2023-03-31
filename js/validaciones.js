a7dbad2a9b0297d2fd3f823ecd9beb744392263c
@josegabo21 <josegabriel2.jr78@gmail.com>
2023-03-31 02:50:00 -0430

    portafolio

---
+++ js/validaciones.js
@@ -0,0 +1,14 @@
+const nombre = document.getElementById('nombre');
+const correo = document.getElementById('email');
+const asunto = document.getElementById('asunto');
+const mensaje = document.getElementById('mensaje');
+const error = document.getElementById('error');
+
+
+function enviarMensaje(){
+
+  return false;
+
+}
+
+
